9d63ece147254d3681e16e16b0c8e40b6a85e0ae
jyanev01 <jordanyanev@gmail.com>
2021-10-03 11:01:51 -0400

    clean file jscript

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,38 +4,23 @@ var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").html(currentDate);
 
 
-// attempt to change color-- StackOverflow help; doesn't work
-// function getHours() {
+//  Change colors with the ohours
 var currentHour = parseInt(moment().format('HH'));
-// };
 
-// currentHour = moment().hour();
-// console.log(currentHour);
 var idArr = [8,9,10,11,12,13,14,15,16,17,18,19]
 for(i=8; i<idArr.length + 8; i++){
-    // console.log($("#text"+i))
-    // console.log(idArr[i-8])
-    // console.log(currentHour)
     if(idArr[i-8] < currentHour){
-        // console.log("past")
         $("#text"+i).attr("class","col-md-10 past")
     }else if(idArr[i-8] == currentHour){
-        // console.log("present")
         $("#text"+i).attr("class","col-md-10 present")
     }else {
-        // console.log("future")
         $("#text"+i).attr("class","col-md-10 future")
     }
+
     // WHen I refresh the page, then the saved text persists
 
     $("#text"+i).val(localStorage.getItem("text"+i))
 }
-
-
-
-
-
-
 
 // SAVE need button to save to local storage-
 $(".saveBtn").on("click", function(){
@@ -45,7 +30,6 @@ $(".saveBtn").on("click", function(){
 
     // save to local storage
     localStorage.setItem(key, value)
-
 });
 
 
